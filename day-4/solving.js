9679bc7609823915f31a7e7478f5fccfdf1750c2
Zakharevich Peter <butthead4z@mail.ru>
2020-12-13 19:31:23 +0300

    Update solving.js
    day 4 part 2

--- day-4/solving.js
+++ day-4/solving.js
@@ -1,12 +1,12 @@
 var src = source();
 var reuiredFieds = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
+    { key: 'byr', validator: byrValidator },
+    { key: 'iyr', validator: iyrValidator },
+    { key: 'eyr', validator: eyrValidator },
+    { key: 'hgt', validator: hgtValidator },
+    { key: 'hcl', validator: hclValidator },
+    { key: 'ecl', validator: eclValidator },
+    { key: 'pid', validator: pidValidator },
 ];
 
 var documents = src.split(/\n/);
@@ -21,16 +21,15 @@ for(var i=0; i< documents.length; i++) {
         
         var fields = docSet.join(" ").split(" ");
         var checkRes = reuiredFieds.every( reqF => {
-            var reqField = fields.find(f=>f.includes(reqF));
+            var reqField = fields.find(f=>f.includes(reqF.key));
             if(!reqField) {
                 return false;
             }
 
             var keyVal = reqField.split(":");
-            return keyVal[1] !== "";
+            return reqF.validator(keyVal[1]);
+            
         });
-
-
 
         if(checkRes) {
             validDocs ++;
@@ -42,3 +41,38 @@ for(var i=0; i< documents.length; i++) {
 }
 
 console.log(validDocs)
+
+function byrValidator(value) {
+    return value >= 1920 && value <= 2002;
+}
+
+function iyrValidator(value) {
+    return value >= 2010 && value <= 2020;
+}
+
+function eyrValidator(value) {
+    return value >= 2020 && value <= 2030;
+}
+
+function hgtValidator(value) {
+    var arr = value.split(/(cm|in)$/);
+    if(arr[1] === 'cm') {
+        return arr[0] >= 150 && arr[0]  <= 193;
+    } else if (arr[1] === 'in') {
+        return arr[0]  >= 59 && arr[0]  <= 76;
+    } 
+    return false;
+}
+
+function hclValidator(value) {
+    return (/^#([a-f0-9]{6})$/).test(value);
+}
+
+function eclValidator(value) {
+    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        .includes(value);
+}
+
+function pidValidator(value) {
+    return (/^[0-9]{9}$/).test(value);
+}
